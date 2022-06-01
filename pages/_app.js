d8b6565564fd97841a3b50eba32497cb2b328363
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-06-01 19:32:23 +0200

    solve problem

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import Amplify from "aws-amplify";
-import config from "./src/aws-exports";
-Amplify.configure({ ...config, ssr: true });
+import awsconfig from "aws-exports.js";
+Amplify.configure({ ...awsconfig, ssr: true });
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
